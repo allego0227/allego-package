9c7eb77d3da483118d5b8f41727f3c15c4d632e8
Allego <allego.vteam@gmail.com>
2024-11-28 10:12:18 +0800

    fix

---
+++ packages/allego/package/src/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Vteam\Package;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Practice extends Controller
+{
+    public static function Practice()
+    {
+        return 'Practice';
+    }
+}
---
+++ packages/vteam/package/src/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Vteam\Package;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Test extends Controller
+{
+    public static function Test()
+    {
+        return 'Test';
+    }
+}
